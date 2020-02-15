c7c865d45eeeaef203868f62e0278fb7aec3f93c
Jay <jay.wang@titansoft.com.sg>
2020-02-15 23:32:22 +0800

    add Nlog and implement CSP in header with report uri

---
+++ ContentSecurityPolicy/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ContentSecurityPolicy.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ContentSecurityPolicy.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			_logger.LogInformation("Test");
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult LogBlockReport([FromBody]CspReportRequest cspReportRequest)
+		{
+			_logger.LogInformation(cspReportRequest.ToString());
+			return Json(true);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+
+	public class CspReportRequest
+	{
+		[JsonProperty("csp-report")]
+		public CspReport CspReport { get; set; }
+
+		public override string ToString()
+		{
+			return $"{nameof(CspReport)}: {CspReport}";
+		}
+	}
+	public class CspReport
+	{
+		[JsonProperty("document-uri")] public string DocumentUri { get; set; }
+		[JsonProperty("referrer")] public string Referrer { get; set; }
+		[JsonProperty("violated-directive")] public string ViolatedDirective { get; set; }
+		[JsonProperty("effective-directive")] public string EffectiveDirective { get; set; }
+		[JsonProperty("original-policy")] public string OriginalPolicy { get; set; }
+		[JsonProperty("blocked-uri")] public string BlockedUri { get; set; }
+		[JsonProperty("status-code")] public int StatusCode { get; set; }
+
+
+		public override string ToString()
+		{
+			return $"{nameof(DocumentUri)}: {DocumentUri}, {nameof(Referrer)}: {Referrer}, {nameof(ViolatedDirective)}: {ViolatedDirective}, {nameof(EffectiveDirective)}: {EffectiveDirective}, {nameof(OriginalPolicy)}: {OriginalPolicy}, {nameof(BlockedUri)}: {BlockedUri}, {nameof(StatusCode)}: {StatusCode}";
+		}
+	}
+
+}
---
+++ ContentSecurityPolicy/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using SameSiteMode = Microsoft.AspNetCore.Http.SameSiteMode;
+
+namespace ContentSecurityPolicy
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.Configure<MvcOptions>(options =>
+			{
+				var inputFormatter = options.InputFormatters.First(m => m.GetType() == typeof(JsonInputFormatter)) as JsonInputFormatter;
+				inputFormatter.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/csp-report"));
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.Use(async (context, next) =>
+			{
+				context.Response.OnStarting(callback: () =>
+				{
+					context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; report-uri /Home/LogBlockReport;");
+					return Task.FromResult(0);
+				});
+				await next();
+			});
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+			
+		}
+	}
+}
